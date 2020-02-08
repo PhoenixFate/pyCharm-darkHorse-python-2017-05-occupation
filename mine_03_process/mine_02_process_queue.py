8485f785f1f13d9324fcff4ac76fc15df7424b4d
phoenix fate <sm516116978@outlook.com>
2020-02-08 14:12:13 +0800

    day02 多进程 finished

--- mine_03_process/mine_02_process_queue.py
+++ mine_03_process/mine_02_process_queue.py
@@ -2,13 +2,41 @@
 from multiprocessing import Queue
 from multiprocessing import Process
 import multiprocessing
+import time
+
+
+def basic_usage():
+    q = Queue(3)
+    print("q.empty(): %s" % q.empty())
+    q.put("message1")
+    q.put("message2")
+    print("q.full(): %s" % q.full())
+    q.put("message3")
+    print("q.full(): %s" % q.full())
+
+    try:
+        q.put("message4", True, 2)
+    except Exception as e:
+        print(e)
+        print("queue已经满了：%s" % q.qsize())
+
+    try:
+        q.put_nowait("message5")
+    except Exception as e:
+        print(e)
+        print("queue已经满了：%s" % q.qsize())
+
+    print(q.get())
+
+    if not q.full():
+        q.put("message7")
 
 
 def download_from_web(q):
     """下载数据"""
     # 模拟从网上下载数据
     download_data = [11, 22, 33, 44]
-
+    time.sleep(1)
     # 向队列中写入数据
     q.put(download_data)
 
@@ -29,6 +57,8 @@ def analyze_data(q):
 
 
 def main():
+    basic_usage()
+
     # 1.创建一个队列
     q = Queue()
     # 2.将队列的引用当作实参传递进去
